9c946ed13cea4f4789e183c9b56794d4f74e85bb
Gabriel Dan Omandam <gomandam@c2r19s6.42madrid.com>
2025-10-11 00:39:48 +0200

    [add] thread *.h

--- philosophers.h
+++ philosophers.h
@@ -6,8 +6,12 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 16:13:16 by gomandam          #+#    #+#             */
-/*   Updated: 2025/10/10 16:13:48 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/10/11 00:39:20 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <pthread.h> // mutex, thread
+
+int	pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr);
+
