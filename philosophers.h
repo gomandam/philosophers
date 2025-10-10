7c51f822de0a28e23b66f609ddb991c93df6a842
gomandam <gomandam@student.42madrid.com>
2025-10-11 00:46:17 +0200

    [add] trial commit

--- philosophers.h
+++ philosophers.h
@@ -6,12 +6,12 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 16:13:16 by gomandam          #+#    #+#             */
-/*   Updated: 2025/10/11 00:39:20 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/10/11 00:45:55 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <pthread.h> // mutex, thread
+#include <pthread.h> // mutex, thread, initialize
 
 int	pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr);
 
