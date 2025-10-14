00e67e7b40182e9d4700738f515ad86d4257c341
gomandam <gomandam@student.42madrid.com>
2025-10-14 03:04:44 +0200

    [add] .h

--- philosophers.h
+++ philosophers.h
@@ -6,12 +6,16 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 16:13:16 by gomandam          #+#    #+#             */
-/*   Updated: 2025/10/11 00:45:55 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/10/14 03:04:16 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
 #include <unistd.h>
 #include <pthread.h> // mutex, thread, initialize
 
 int	pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr);
 
+#endif
