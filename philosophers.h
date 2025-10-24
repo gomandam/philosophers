0582d1549c49f979db35e2fdd49009556a403b6f
gomandam <gomandam@student.42madrid.com>
2025-10-24 20:03:41 +0200

    [add] philosophers.h structs

--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 16:13:16 by gomandam          #+#    #+#             */
-/*   Updated: 2025/10/14 03:04:16 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/10/24 20:03:13 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 
 #include <unistd.h>
 #include <pthread.h> // mutex, thread, initialize
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <sys/time.h>
+
+typedef struct s_mutex
+{
+	//
+}		t_mutex
+
+
 
 int	pthread_mutex_init(pthread_mutex_t *restrict mutex, const pthread_mutexattr_t *restrict attr);
 
