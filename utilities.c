67910046cc930989b770e2510a681dcf6af9a568
gomandam <gomandam@student.42madrid.com>
2025-10-29 00:12:52 +0100

    [add] utilities

--- utilities.c
+++ utilities.c
@@ -6,7 +6,7 @@
 /*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 23:33:17 by gomandam          #+#    #+#             */
-/*   Updated: 2025/10/28 23:50:33 by gomandam         ###   ########.fr       */
+/*   Updated: 2025/10/29 00:12:27 by gomandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,35 @@ int	ft_isdigit(char c)
 	return (0);
 }
 
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t'
+		|| c == '\v')
+		return (1);
+	return (0);
+}
 
+// redefined atoi()
+int	ft_atoi(char *str)
+{
+	unsigned long long	nb;
+	int					sign;
+	int					i;
+
+	nb = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
+	return (sign * nb);
+}
