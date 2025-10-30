80dab2a1ef7a4e8d5a82e6137f85b30ee956cbea
gomandam <gomandam@student.42madrid.com>
2025-10-30 01:35:50 +0100

    [add] notification.c

---
+++ notification.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   notification.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gomandam <gomandam@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 16:13:16 by gomandam          #+#    #+#             */
+/*   Updated: 2025/10/30 01:33:08 by gomandam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.c"
+
+// TYPE_*** should be an enum or struct?
+// refer to: https://github.com/Glagan/42-Philosophers/tree/master/philo_three
+static char
+	*notifications(int type)
+{
+	if (type == )
+		return (" eating process.\n");
+	else if (type == )
+		return (" sleeping state.\n");
+
+}
